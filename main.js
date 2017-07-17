24c142e07aab8b060362539ef05ea72bb4b126bd
GermanBluefox <dogafox@gmail.com>
2017-07-17 18:35:53 +0200

    (bluefox) working on fullcalendar

--- main.js
+++ main.js
@@ -8,25 +8,26 @@ var utils     = require(__dirname + '/lib/utils'); // Get common adapter utils
 var timeUtils = require(__dirname + '/admin/utils.js');
 var adapter   = new utils.Adapter('fullcalendar');
 var later     = require('later');
-var SunCalc   = require(__dirname + '/admin/suncalc.js');
 var events;
 var nextTimer;
-var systemConfig;
+var allEvents = [];
+var configuredEvents = [];
+var names = {};
+var cfgEventsSettings = {};
+var rooms = {};
+var funcs = {};
 
 adapter.on('objectChange', function (id, obj) {
     if (!id) return;
 
-    if (id === 'system.config') {
-        if (obj) systemConfig = obj;
-
-        if (systemConfig.common.latitude  === undefined || systemConfig.common.latitude  === null ||
-            systemConfig.common.longitude === undefined || systemConfig.common.longitude === null) {
-            adapter.log.warn('Please specify longitude and latitude in system settings, elsewise astro events will not work');
+    if (names[id]) {
+        delete names[id];
+        if (obj) {
+            getStateName(id);
         }
-
-        calculateNext();
-        return;
     }
+
+    if (id.substring(0, adapter.namespace.length + 1) !== adapter.namespace + '.') return;
 
     if (events[id]) {
         stopEvent(events[id]);
@@ -38,7 +39,90 @@ adapter.on('objectChange', function (id, obj) {
     calculateNext();
 });
 
+function getStateName(id, state, callback) {
+    if (names[id]) {
+        var name = names[id];
+        if (state) {
+            name += ' = ' + state.val;
+        }
+
+        callback && callback(names[id], id, state);
+    } else {
+        adapter.getForeignState(id, function (err, obj) {
+            var enums = getRoomFunc(id);
+            if (enums.func && enums.room) {
+                names[id] = enums.func + ' / ' + enums.room;
+            } else
+            if (obj && obj.common && obj.common.name) {
+                names[id] = obj.common.name;
+            }
+
+            name = names[id] || id;
+            if (state) {
+                name += ' = ' + state.val;
+                // generate for window/door/light/dimmer automatically opened/closed/set to
+            }
+            callback && callback(name, id, state);
+        });
+    }
+}
+
+adapter.on('stateChange', function (id, state) {
+    if (id && state) {
+        if (id.match('^system\.')) return;
+        if (adapter.config.allEventsEnabled || adapter.config.cfgEventsEnabled) {
+            getStateName(id, state, function (name, id, state) {
+                if (adapter.config.allEventsEnabled) {
+                    if (adapter.config.ackType === 'true' && !state.ack) {
+                        return;
+                    }
+                    if (adapter.config.ackType === 'false' && state.ack) {
+                        return;
+                    }
+                    var cfgA = {id: id, val: state.val, ack: state.ack, ts: state.ts, name: name};
+                    allEvents.push(cfgA);
+                    if (allEvents.length > adapter.config.allEventsMax) {
+                        allEvents.slice(allEvents.length - adapter.config.allEventsMax);
+                    }
+                    adapter.setState('info.lastEvent', JSON.stringify(cfgA), true);
+                }
+
+                if (adapter.config.cfgEventsEnabled) {
+                    if (cfgEventsSettings[id]) {
+                        if (cfgEventsSettings[id].ackType === 'true' && !state.ack) {
+                            return;
+                        }
+                        if (cfgEventsSettings[id].ackType === 'false' && state.ack) {
+                            return;
+                        }
+                        var cfgE = {id: id, val: state.val, ack: state.ack, ts: state.ts, name: name};
+                        configuredEvents.push(cfgE);
+                        if (configuredEvents.length > adapter.config.cfgEventsMax) {
+                            configuredEvents.slice(configuredEvents.length - adapter.config.cfgEventsMax);
+                        }
+                        adapter.setState('info.lastConfiguredEvent', JSON.stringify(cfgE), true);
+                    }
+                }
+            });
+        }
+    }
+});
+
 adapter.on('ready', main);
+
+adapter.on('message', function (obj) {
+    if (obj && obj.callback) {
+        switch (obj.command) {
+            case 'getAll':
+                adapter.sendTo(obj.from, obj.command, allEvents, obj.callback);
+                break;
+
+            case 'getConfigured':
+                adapter.sendTo(obj.from, obj.command, configuredEvents, obj.callback);
+                break;
+        }
+    }
+});
 
 function stopEvent(event) {
     if (event.timer) {
@@ -48,7 +132,6 @@ function stopEvent(event) {
 }
 
 function executeEvent(event, now) {
-    adapter.log.debug('executeEvent[' + event.common.name + ']: ' + JSON.stringify(event.native));
     event.lastExec = now || new Date().getTime();
     adapter.getForeignObject(event.native.oid, function (err, obj) {
         if (!obj) {
@@ -134,22 +217,12 @@ function calculateNext() {
         // if daily
         if (event.native.cron) {
             if (!event.parsed) {
-                if (event.native.astro) {
-                    // take last second of this day
-                    event.parsed = later.parse.cron(event.native.cron.replace(/^\d\d? \d\d? /, '59 59 23 '), true);
-                } else {
-                    event.parsed = later.parse.cron(event.native.cron);
-                }
+                event.parsed = later.parse.cron(event.native.cron);
             }
             var date = later.schedule(event.parsed).next();
 
-            if (event.native.astro) {
-                date = timeUtils.getAstroTime(event.native.astro, event.native.offset, date, SunCalc, systemConfig);
-            }
-
             if (date) {
-                diff = date.getTime() - nowTick;
-                if (diff >= -2000 && diff < 2000) {
+                if (date.getTime() - nowTick < 2000) {
                     executeEvent(event, nowTick);
                     date = later.schedule(event.parsed).next(1, new Date(nowTick + 2000));
                 }
@@ -164,25 +237,10 @@ function calculateNext() {
             }
         } else { // once
             // expected 2017-09-12T12:12:00
-            var time;
-            if (event.native.astro) {
-                time = timeUtils.getAstroTime(event.native.astro, event.native.offset, timeUtils.parseISOLocal(event.native.start), SunCalc, systemConfig);
-            } else {
-                time = timeUtils.parseISOLocal(event.native.start);
-            }
-
-            if (!time) continue;
-
-            if (nowObj.getFullYear() === time.getFullYear() &&
-                nowObj.getMonth()    === time.getMonth()    &&
-                nowObj.getDate()     === time.getDate()     &&
-                nowObj.getHours()    === time.getHours()    &&
-                nowObj.getMinutes()  === time.getMinutes()  &&
-                (Math.abs(nowObj.getSeconds() - time.getSeconds()) < 2) &&
-                (!event.lastExec || nowTick - event.lastExec > 1999)) {
+            if (event.native.start === nowStr && (!event.lastExec || nowTick - event.lastExec > 1999)) {
                 executeEvent(event, nowTick);
             } else {
-                time = time.getTime();
+                var time = timeUtils.parseISOLocal(event.native.start).getTime();
 
                 diff = time - nowTick;
                 if (diff > 0 && (timeout === null || diff < timeout)) {
@@ -216,8 +274,62 @@ function checkEvent(event) {
     return event;
 }
 
+function getRoomFunc(id) {
+    var room = '';
+    for (var r in rooms) {
+        if (rooms[r].common.members.indexOf(id) !== -1) {
+            room = rooms[r].common.name || r.substring('enum.rooms.'.length);
+            break;
+        }
+    }
+    var func = '';
+    for (var f in funcs) {
+        if (funcs[r].common.members.indexOf(id) !== -1) {
+            func = funcs[r].common.name || r.substring('enum.functions.'.length);
+            break;
+        }
+    }
+    return {func: func, room: room};
+}
+
+function readEnums(cb) {
+    adapter.objects.getObjectView('system', 'enum', {startkey: 'enum.rooms.', endkey: 'enum.rooms.\u9999'}, function (err, res) {
+        rooms = {};
+        for (var r = 0; r < res.rows.length; r++) {
+            if (res.rows[r].value && res.rows[r].value.common && res.rows[r].value.members) {
+                rooms[res.rows[r].id] = res.rows[r].value;
+            }
+        }
+        adapter.objects.getObjectView('system', 'enum', {startkey: 'enum.functions.', endkey: 'enum.functions.\u9999'}, function (err, res) {
+            funcs = {};
+            for (var r = 0; r < res.rows.length; r++) {
+                if (res.rows[r].value && res.rows[r].value.common && res.rows[r].value.members) {
+                    funcs[res.rows[r].id] = res.rows[r].value;
+                }
+            }
+
+            cb && cb();
+        });
+    });
+}
+
+function afterMain(count) {
+    readEnums(function () {
+        adapter.subscribeObjects('*');
+        calculateNext();
+
+        if (count > 100 || adapter.config.allEventsEnabled) {
+            adapter.subscribeForeignStates('*');
+        } else {
+            for (var id in cfgEventsSettings) {
+                adapter.subscribeForeignStates(id);
+            }
+        }
+    });
+}
 function main() {
     later.date.localTime();
+
     adapter.objects.getObjectView('schedule', 'schedule', {startkey: adapter.namespace + '.', endkey: adapter.namespace + '.\u9999'}, function (err, res) {
         events = {};
         if (!err && res) {
@@ -226,16 +338,21 @@ function main() {
                 events[res.rows[i].value._id] = checkEvent(res.rows[i].value);
             }
         }
-        adapter.getForeignObject('system.config', function (err, obj) {
-            systemConfig = obj;
-            if (systemConfig.common.latitude  === undefined || systemConfig.common.latitude  === null ||
-                systemConfig.common.longitude === undefined || systemConfig.common.longitude === null) {
-                adapter.log.warn('Please specify longitude and latitude in system settings, elsewise astro events will not work');
-            }
-
-            adapter.subscribeObjects('*');
-            adapter.subscribeForeignObjects('system.config');
-            calculateNext();
-        });
+        if (adapter.config.cfgEventsEnabled) {
+            adapter.objects.getObjectView('custom', 'state', {startkey: '', endkey: '\u9999'}, function (err, res) {
+                var count = 0;
+                if (!err && res) {
+                    for (var i = 0; i < res.rows.length; i++) {
+                        if (res.rows[i].value[adapter.namespace] && res.rows[i].value[adapter.namespace].enabled) {
+                            cfgEventsSettings[res.rows[i].id] = res.rows[i].value;
+                            count++;
+                        }
+                    }
+                }
+                afterMain(count);
+            });
+        } else {
+            afterMain(0);
+        }
     });
 }
